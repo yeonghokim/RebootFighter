9a94fed36e087ddb402ed2124352344fbdd502e1
yeonghokim <kim-yeongho@kim-yeonghoui-MacBook-Pro.local>
2016-10-28 22:28:42 +0900

    #2

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -4,16 +4,13 @@ USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    // 'scene' is an autorelease object
+
     auto scene = Scene::create();
     
-    // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
-    // add layer as a child to scene
     scene->addChild(layer);
 
-    // return the scene
     return scene;
 }
 
